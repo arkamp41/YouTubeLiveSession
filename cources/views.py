bf1378c74cb90024f88a4cefa42968c527a21a1c
Arkam sayyed <sayyedarkam.p.41@gmail.com>
2023-07-05 20:28:37 +0530

    Docker file path change

--- cources/views.py
+++ cources/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(r):
-    return HttpResponse('<h1>Welcom To Live project deployment</h1>')
+    return HttpResponse('<h1>Welcom Ritesh To Live project deployment</h1>')
